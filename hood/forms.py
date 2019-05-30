4e8253356ec7f5aa89c3b515ab54df01ae129805
somoire <rayr30667@gmail.com>
2019-05-30 14:34:26 -0400

    blogpost form created

--- hood/forms.py
+++ hood/forms.py
@@ -10,3 +10,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model=BlogPost
+        exclude=['username','neighbourhood','avatar']
