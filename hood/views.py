83198c22704728e962e7ee258fe35c520f7a6a4e
somoire <rayr30667@gmail.com>
2019-05-30 14:44:55 -0400

    creating the views

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import neighbourhood,healthservices,Business,Health,Authorities,BlogPost,Profile,notifications,Comment
+from .email import send_priority_email
+from .forms import notificationsForm,ProfileForm,BlogPostForm,BusinessForm,CommentForm
+from decouple import config,Csv
+import datetime as dt
+from django.http import JsonResponse
+import json
+from django.db.models import Q
+from django.contrib.auth.models import User
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
