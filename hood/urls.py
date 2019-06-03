0df745d7e8765bf57d0157e37f92833fb4834823
somoire <rayr30667@gmail.com>
2019-06-03 02:24:34 -0400

    Changed profile

--- hood/urls.py
+++ hood/urls.py
@@ -19,7 +19,7 @@ urlpatterns=[
     url(r'^new/notification$',views.new_notification, name='new-notification'),
     url(r'^update/profile$',views.update_profile, name='update-profile'),
     url(r'^search/',views.search_results, name='search_results'),
-    url(r'^create/profile$',views.create_profile, name='create-profile'),
+
     
 
 ]
